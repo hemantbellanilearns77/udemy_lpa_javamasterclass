413ee5979c9f04164e921a81a55eb82b4d087721
Hemant Bellani <hemantbellanilearns77@gmail.com>
2025-08-08 10:19:21 +0530

    added fixes to the URL

--- misc_utils/src/main/java/com/hb/study/demo_stubs/DemoClassTemplate.java
+++ misc_utils/src/main/java/com/hb/study/demo_stubs/DemoClassTemplate.java
@@ -3,7 +3,8 @@ package com.hb.study.demo_stubs;
 import com.hb.study.udemylpajavamasterclass.global.utils.ExcecutionUtil;
 import com.hb.study.udemylpajavamasterclass.global.utils.ConsoleStyler;
 
-import java.util.Random;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * created by : heman on 24-07-2025, 09:27 am, in the "udemy_lpa_javamasterclass" project
@@ -17,6 +18,7 @@ public class DemoClassTemplate {
     public static void main(String[] ignoredArgs) {
         execution.initialize();
         ConsoleStyler.styleInitializationInfo("This and this was initialized and will be used throughout this program demo"); // optional
+        demoFunction();
         execution.finalizeExecution();
     }
 
@@ -27,12 +29,11 @@ public class DemoClassTemplate {
         ConsoleStyler.styleIntro("Welcome to the subsection X"); // optional
         ConsoleStyler.styleInitializationInfo("This and this was initialized and will be used throughout this subsection section X demo"); // optional
         ConsoleStyler.styleExecutionInsight("Regarding this sub-section.... some insights "); // optional
-        /* for simply printing elements , no sorting, no uppercase, no formatting on numbers etc...
-            ConsoleStyler.styleEachAsIs("Label Prefix, like Name/Guest/Part: ",List<String>);
-        */
-        /* for printing elements and has all options:  sorting, uppercase, formatting on numbers etc...
-            ConsoleStyler.styleEach("Label Prefix, like Name/Guest/Part: ",List<String>);
-        */
+        List<String> dummyList= new ArrayList<>();
+         //for simply printing elements , no sorting, no uppercase, no formatting on numbers etc...
+            ConsoleStyler.styleEachAsIs("Label Prefix, like Name/Guest/Part: ",dummyList);
+         //for printing elements and has all options:  sorting, uppercase, formatting on numbers etc...
+            ConsoleStyler.styleEach("Label Prefix, like Name/Guest/Part: ",dummyList,true, true, true);
         //ConsoleStyler.styleIt(custom styling... );
         ConsoleStyler.styleOutput("Output Heading", "Plain output "); //  output
         ConsoleStyler.halfDivider();//This is required only if there is another subsection....
