790ec0661a02154adee18c85e02824b2fb7b7c98
Hemant Bellani <hemantbellanilearns77@gmail.com>
2025-08-08 20:56:50 +0530

    sonar fixes

--- src/main/java/com/hb/study/udemylpajavamasterclass/section7_oop_part1/coding_challenges/oopsbasicschallenge/Car.java
+++ src/main/java/com/hb/study/udemylpajavamasterclass/section7_oop_part1/coding_challenges/oopsbasicschallenge/Car.java
@@ -1,20 +1,67 @@
 package com.hb.study.udemylpajavamasterclass.section7_oop_part1.coding_challenges.oopsbasicschallenge;
 
-class Car extends Vehicle{
+public class Car {
 
-    private int numberOfGears;
-    private int currentlyActivatedGear;
-    private int numberOfWheels;
-    private int numberOfDoors;
-    private boolean isManual;
+    private String make = "Tesla";
+    private String model = "Model X";
+    private String color = "Gray";
+    private int doors = 2;
+    private boolean convertible = true;
 
+    public String getMake() {
+        return make;
+    }
 
-    public Car(String name, String size, int currentSpeed, int currentDirection, int numberOfWheels, int maxSpeed, int numberOfGears, int currentlyActivatedGear) {
-        super(name, size, currentSpeed, currentDirection, maxSpeed);
-        this.numberOfGears = numberOfGears;
-        this.currentlyActivatedGear = currentlyActivatedGear;
-        this.numberOfWheels = numberOfWheels;
-        this.numberOfDoors = numberOfDoors;
-        this.isManual = isManual;
+    public String getModel() {
+        return model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getDoors() {
+        return doors;
+    }
+
+    public boolean isConvertible() {
+        return convertible;
+    }
+
+    public void setMake(String make) {
+
+        if (make == null) make = "Unknown";
+        String lowercaseMake = make.toLowerCase();
+        switch (lowercaseMake) {
+            case "holden", "porsche", "tesla" -> this.make = make;
+            default -> {
+                this.make = "Unsupported ";
+            }
+        }
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setDoors(int doors) {
+        this.doors = doors;
+    }
+
+    public void setConvertible(boolean convertible) {
+        this.convertible = convertible;
+    }
+
+    public void describeCar() {
+
+        System.out.println(doors + "-Door " +
+                color + " " +
+                make + " " +
+                model + " " +
+                (convertible ? "Convertible" : ""));
     }
 }
