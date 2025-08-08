de753b65c366588ef89b62291e6ec370a6856d73
Hemant Bellani <hemantbellanilearns77@gmail.com>
2025-08-08 20:56:50 +0530

    sonar fixes

--- src/main/java/com/hb/study/udemylpajavamasterclass/section7_oop_part1/coding_challenges/oopsbasicschallenge/Main.java
+++ src/main/java/com/hb/study/udemylpajavamasterclass/section7_oop_part1/coding_challenges/oopsbasicschallenge/Main.java
@@ -1,9 +1,26 @@
 package com.hb.study.udemylpajavamasterclass.section7_oop_part1.coding_challenges.oopsbasicschallenge;
 
 public class Main {
+
     public static void main(String[] args) {
-        //ToDo write code here
 
+        Car car = new Car();
+        car.setMake("Porsche");
+        car.setModel("Carrera");
+        car.setDoors(2);
+        car.setConvertible(true);
+        car.setColor("black");
+        System.out.println("make = " + car.getMake());
+        System.out.println("model = " + car.getModel());
+        car.describeCar();
+
+        Car targa = new Car();
+        targa.setMake("Porsche");
+        targa.setModel("Targa");
+        targa.setDoors(2);
+        targa.setConvertible(false);
+        targa.setColor("red");
+
+        targa.describeCar();
     }
-
 }
