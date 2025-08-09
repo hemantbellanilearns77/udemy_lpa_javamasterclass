17fc9a52e29f30a7646c94619854907f84a68c7b
Hemant Bellani <hemantbellanilearns77@gmail.com>
2025-08-09 23:12:34 +0530

    restored

---
+++ src/test/java/com/hb/study/udemylpajavamasterclass/section3_firststeps/coding_challenges/CompoudAssignmentOperatorChallangeTest.java
@@ -0,0 +1,13 @@
+package com.hb.study.udemylpajavamasterclass.section3_firststeps.coding_challenges;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CompoudAssignmentOperatorChallangeTest {
+
+    @Test
+    void testMainRuns() {
+        assertDoesNotThrow(() -> CompoudAssignmentOperatorChallange.main(new String[]{}));
+    }
+}
---
+++ src/test/java/com/hb/study/udemylpajavamasterclass/section5_buildingblocks/coding_challenges/OverloadingChallengeTest.java
@@ -0,0 +1,31 @@
+package com.hb.study.udemylpajavamasterclass.section5_buildingblocks.coding_challenges;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * created by : heman on 06-08-2025, 11:52 am, in the "run-pmd.bat" project
+ **/
+class OverloadingChallengeTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void main() {
+    }
+
+    @Test
+    void convertToCentimeters() {
+    }
+
+    @Test
+    void testConvertToCentimeters() {
+    }
+}
