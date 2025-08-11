000d3c18d2109266e005d0466991769af86ce450
Hemant Bellani <hemantbellanilearns77@gmail.com>
2025-08-11 11:31:45 +0530

    sonar-fixes

--- src/main/java/com/hb/study/udemy_lpa_javamasterclass/section10_collections/coding_challenges/linkedlistchallenge/LinkedListChallenge_Main.java
+++ src/main/java/com/hb/study/udemy_lpa_javamasterclass/section10_collections/coding_challenges/linkedlistchallenge/LinkedListChallenge_Main.java
@@ -1,33 +1,28 @@
 package com.hb.study.udemy_lpa_javamasterclass.section10_collections.coding_challenges.linkedlistchallenge;
 
 import com.hb.study.udemy_lpa_javamasterclass.global.utils.ConsoleStyler;
+import com.hb.study.udemy_lpa_javamasterclass.global.utils.ExcecutionUtil;
 
 import java.util.LinkedList;
 import java.util.ListIterator;
 import java.util.Scanner;
 
 public class LinkedListChallenge_Main {
-    public static String asteriskSeparatorLine = "*".repeat(153) + "\n";
-    public static String programOutputBegins = "\n" + "*".repeat(54) + " The Output of " +
-            LinkedListChallenge_Main.class.getSimpleName() + " is as below " + "*".repeat(54) + "\n";
 
-
+    public static final ExcecutionUtil execution = new ExcecutionUtil();
     public static void main(String[] args) {
-
+        execution.initialize();
         LinkedList<Town> itinerary = new LinkedList<>();
-        ConsoleStyler.styleOutput(programOutputBegins);
+
 
         createItineray(itinerary);
-        ConsoleStyler.styleOutput("\nThe list before any user performed operation using the Menu is as below:");
-
-        ConsoleStyler.styleOutput(itinerary.toString());
-        ConsoleStyler.styleOutput(asteriskSeparatorLine);
+        ConsoleStyler.styleInitializationInfo("\nThe list before any user performed operation using the Menu is as below:\n" + itinerary  );
+        ConsoleStyler.halfDivider();
         var iterator = itinerary.listIterator();
         boolean forward = true;
-        //executeMenuCommands();
         Scanner scanner = new Scanner(System.in);
         String nextInput;
-        do{
+        do {
             if (!iterator.hasPrevious()) {
                 ConsoleStyler.styleOutput("Originating : " + iterator.next());
                 forward = true;
@@ -38,113 +33,92 @@ public class LinkedListChallenge_Main {
             }
             printMenu();
             nextInput = scanner.nextLine().trim().toUpperCase();
-            switch(nextInput){
-                case "F" ->  {
-                    ConsoleStyler.styleOutput("User wants to go forward");
-                    if (!forward) {           // Reversing Direction
-                        forward = true;
-                        if (iterator.hasNext()) {
-                            iterator.next();  // Adjust position forward
-                        }
-                    }
-                    if (iterator.hasNext()) {
-                        ConsoleStyler.styleOutput(iterator.next().toString());
-                    }
-                    break;
+            switch (nextInput) {
+                case "F" -> {
+                    forward = isABoolean(forward, iterator);
                 }
-                case "B" ->  {
-                    ConsoleStyler.styleOutput("User wants to go backwards");
-                    if (forward) {           // Reversing Direction
-                        forward = false;
-                        if (iterator.hasPrevious()) {
-                            iterator.previous();  // Adjust position backwards
-                        }
-                    }
-
-                    if (iterator.hasPrevious()) {
-                        ConsoleStyler.styleOutput(iterator.previous().toString());
-                    }
-                    break;
+                case "B" -> {
+                    forward = isForward(forward, iterator);
                 }
-                case "L" -> {listPlaces(itinerary); break;}
-                case "M" -> {continue;}
-                case "Q" -> {break;}
-                default -> {
-                    ConsoleStyler.styleOutput("*".repeat(27) + " Extremely Sorry, but this is an invalid input. try again.... " + "*".repeat(27) + "\n");
-                    break;
+                case "L" -> {
+                    listPlaces(itinerary);
                 }
+                case "M" -> {
+                    printMenu();
+                }
+                case "Q" -> {
+                }
+                default ->
+                        ConsoleStyler.styleOutput("*".repeat(27) + " Extremely Sorry, but this is an invalid input. try again.... " + "*".repeat(27) + "\n");
             }
         } while (!nextInput.equalsIgnoreCase("Q"));
+        execution.finalizeExecution();
+    }
 
+    private static boolean isABoolean(boolean forward, ListIterator<Town> iterator) {
+        ConsoleStyler.styleOutput("User wants to go forward");
+        if (!forward) {           // Reversing Direction
+            forward = true;
+            if (iterator.hasNext()) {
+                iterator.next();  // Adjust position forward
+            }
+        }
+        if (iterator.hasNext()) {
+            ConsoleStyler.styleOutput(iterator.next().toString());
+        }
+        return forward;
+    }
 
+    private static boolean isForward(boolean forward, ListIterator<Town> iterator) {
+        ConsoleStyler.styleOutput("User wants to go backwards");
+        if (forward) {           // Reversing Direction
+            forward = false;
+            if (iterator.hasPrevious()) {
+                iterator.previous();  // Adjust position backwards
+            }
+        }
+        if (iterator.hasPrevious()) {
+            ConsoleStyler.styleOutput(iterator.previous().toString());
+        }
+        return forward;
     }
 
     private static void listPlaces(LinkedList<Town> itinerary) {
         ConsoleStyler.styleOutput("User wants to list our the itinerary and its as below: \n ");
         ConsoleStyler.styleOutput(itinerary.toString());
-        ConsoleStyler.styleOutput(asteriskSeparatorLine);
+        ConsoleStyler.divider();
     }
-
-
 
     private static void iterateForwards(LinkedList<Town> itinerary) {
         ConsoleStyler.styleOutput("User wants to go forward.... ");
-
     }
-
-   /* private static void executeMenuCommands(LinkedList<Town> itinerary) {
-        Scanner scanner = new Scanner(System.in);
-        String nextInput;
-        do{
-
-            nextInput = scanner.nextLine();
-            switch(nextInput){
-                case "F" ->  {iterateForwards(itinerary); printMenu();}
-                case "B" ->  {iterateBackwards(itinerary); printMenu();}
-                case "L" -> {listPlaces(itinerary); printMenu();}
-                case "M" -> {printMenu();}
-                case "Q" -> {break;}
-                default -> {
-                    ConsoleStyler.styleOutput("Sorry, invalid input ");
-                    break;}
-            }
-        } while (!nextInput.equalsIgnoreCase("Q"));
-
-    }*/
 
     private static void createItineray(LinkedList<Town> itinerary) {
-
-        addTown(new Town("Perth", 3923),itinerary);
-        addTown(new Town("Brisbane", 917),itinerary);
-        addTown(new Town("Sydney",0), itinerary);
-        addTown(new Town("Alice Springs", 2771),itinerary);
-        addTown(new Town("Melbourne", 877),itinerary);
-        addTown(new Town("Adelaide", 1374),itinerary);
-        addTown(new Town("Darwin", 3972),itinerary);
-       addTown(new Town("brisbaNe", 917),itinerary);
-
-
+        addTown(new Town("Perth", 3923), itinerary);
+        addTown(new Town("Brisbane", 917), itinerary);
+        addTown(new Town("Sydney", 0), itinerary);
+        addTown(new Town("Alice Springs", 2771), itinerary);
+        addTown(new Town("Melbourne", 877), itinerary);
+        addTown(new Town("Adelaide", 1374), itinerary);
+        addTown(new Town("Darwin", 3972), itinerary);
+        addTown(new Town("brisbaNe", 917), itinerary);
     }
-
 
     private static void addTown(Town townToAdd, LinkedList<Town> itinerary) {
         ListIterator<Town> itineraryIterator = itinerary.listIterator();
         Town currentTownAtCursor;
-        if(itinerary.contains(townToAdd) || checkIfTownExists(townToAdd, itinerary)) {
+        if (itinerary.contains(townToAdd) || checkIfTownExists(townToAdd, itinerary)) {
             ConsoleStyler.styleOutput("*Please Note: Duplicate entry attempt found for \'" + townToAdd.town() + "\', so not adding it!");
-            return;
         } else {
 
-            if(itinerary.size() != 0){
-                while(itineraryIterator.hasNext()) {
+            if (!itinerary.isEmpty()) {
+                while (itineraryIterator.hasNext()) {
                     currentTownAtCursor = itineraryIterator.next();
-                    //if(itineraryIterator.hasNext()) {
-                        if(townToAdd.distanceFromOrigin() < currentTownAtCursor.distanceFromOrigin()){
-                            itineraryIterator.previous();
-                            itineraryIterator.add(townToAdd);
-                            return;
-                        }
-                    //}
+                    if (townToAdd.distanceFromOrigin() < currentTownAtCursor.distanceFromOrigin()) {
+                        itineraryIterator.previous();
+                        itineraryIterator.add(townToAdd);
+                        return;
+                    }
                 }
                 itineraryIterator.add(townToAdd);
             } else {
@@ -157,10 +131,10 @@ public class LinkedListChallenge_Main {
         boolean duplicateFound = false;
         ListIterator<Town> itineraryIterator = itinerary.listIterator();
         Town nextTown = null;
-        if(itinerary.size() > 0) {
-            while(itineraryIterator.hasNext()) {
+        if (!itinerary.isEmpty()) {
+            while (itineraryIterator.hasNext()) {
                 nextTown = itineraryIterator.next();
-                if(nextTown.town().equalsIgnoreCase(townToAdd.town())) {
+                if (nextTown.town().equalsIgnoreCase(townToAdd.town())) {
                     duplicateFound = true;
                 }
             }
@@ -168,19 +142,15 @@ public class LinkedListChallenge_Main {
         return duplicateFound;
     }
 
-    private static void removeTown(Town townToRemove, LinkedList<Town> itinerary){
-        //itinerary.remove(town)
-    }
-
-    private static void printMenu(){
+    private static void printMenu() {
         String menuTextBlock = "*".repeat(27) + " Available actions (select letter) " + "*".repeat(27) + "\n" +
                 """ 
-                (F)orward - to iterate forwards
-                (B)ackward - to iterate backwards
-                (L)ist Places - to print the itinerary
-                (M)enu - to reprint the Menu items
-                (Q)uit - to Quit 
-                Enter Value: """;
+                        (F)orward - to iterate forwards
+                        (B)ackward - to iterate backwards
+                        (L)ist Places - to print the itinerary
+                        (M)enu - to reprint the Menu items
+                        (Q)uit - to Quit 
+                        Enter Value: """;
         System.out.print(menuTextBlock);
     }
 }
