02c25447b3f2251379c600dacb57fef1a9b9228a
Hemant Bellani <hemantbellanilearns77@gmail.com>
2025-08-15 15:19:06 +0530

    sonar fixes

--- misc_utils/src/main/java/com/hb/study/misc_utils/demo_stubs/DemoClassTemplate.java
+++ misc_utils/src/main/java/com/hb/study/misc_utils/demo_stubs/DemoClassTemplate.java
@@ -1,38 +1,47 @@
 package com.hb.study.misc_utils.demo_stubs;
 
+import com.hb.study.udemy_lpa_javamasterclass.global.constants.CommonConstants;
 import com.hb.study.udemy_lpa_javamasterclass.global.utils.ExcecutionUtil;
 import com.hb.study.udemy_lpa_javamasterclass.global.utils.ConsoleStyler;
 import com.hb.study.udemy_lpa_javamasterclass.global.utils.NamesUtil;
 
+import java.security.SecureRandom;
 import java.util.List;
 
 /**
- * created by : heman on 13-08-2025, 08:01 pm, in the "run-pmd.bat" project
+ * created by : heman on 15-08-2025, 03:18 pm, in the "run-pmd.bat" project
  **/
-public class DemoClassTemplate {
-
-    //Object level or Static declarations here...
+public class DemoClassTemplate {//Object level or Static declarations here...
     private static final ExcecutionUtil execution = new ExcecutionUtil();
+    private static final SecureRandom secureRandom = new SecureRandom();
+    private static final NamesUtil namesUtil = new NamesUtil(CommonConstants.MAX_ITERATION_COUNT);
 
     public static void main(String[] ignoredArgs) {
         execution.initialize();
         ConsoleStyler.styleInitializationInfo("This and this was initialized and will be used throughout this program demo"); // optional
         // main method initializations.... 
-
+        /*
+         *****************************************************
+         */
         //your own code here; recommended to divide in function calls
         demoFunction();
-
+        /*
+         *****************************************************
+         */
         execution.finalizeExecution();
     }
 
     public static void demoFunction() {
-        NamesUtil namesUtil = new NamesUtil(9);
         ConsoleStyler.startSection("Welcome to Demo Section"); // required
         ConsoleStyler.styleInitializationInfo("This and this was initialized and will be used throughout this section demo"); // optional
         ConsoleStyler.halfDivider();
         ConsoleStyler.styleIntro("Welcome to the subsection X"); // optional
         ConsoleStyler.styleInitializationInfo("This and this was initialized and will be used throughout this subsection section X demo"); // optional
-        ConsoleStyler.styleExecutionInsight("Regarding this sub-section.... some insights "); // optional
+        ConsoleStyler.styleExecutionInsight("""
+                Regarding this sub-section.... some insights...
+                Example you can generate random values using Secure Random"
+                """); // optional
+        ConsoleStyler.styleOutput("Random value generated between 1 and 9 using SecureRandom : " + secureRandom.nextInt(1, 10));
         //for simply printing elements , no sorting, no uppercase, no formatting on numbers etc...
         ConsoleStyler.styleEachAsIs("Label Prefix, like Name/Guest/Part: ", List.of(namesUtil.getDEFAULT_FIRST_NAMES()));
 
