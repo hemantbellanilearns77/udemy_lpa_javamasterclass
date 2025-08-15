637f8392c604009ee0872c492a8cd7247084c2a3
Hemant Bellani <hemantbellanilearns77@gmail.com>
2025-08-15 14:18:01 +0530

    sonar fixes

--- src/main/java/com/hb/study/udemy_lpa_javamasterclass/section10_collections/demostubs/LinkedListVsArrayList.java
+++ src/main/java/com/hb/study/udemy_lpa_javamasterclass/section10_collections/demostubs/LinkedListVsArrayList.java
@@ -8,6 +8,9 @@ import java.util.LinkedList;
 public class LinkedListVsArrayList {
 
     public static final ExcecutionUtil execution = new ExcecutionUtil();
+    private static final String BRISBANE = "Brisbane";
+    private static final String WAS_REMOVED_FROM_PLACES_TO_VISIT = "was removed from placesToVisit.";
+    private static final String PLACES_TO_VISIT_AFTER_A_CALL_TO = "Places to visit after a call to";
 
     public static void main(String[] args) {
         execution.initialize();
@@ -17,26 +20,26 @@ public class LinkedListVsArrayList {
         placesToVisit.add("Sydney");
         placesToVisit.addFirst("Canberra");
         ConsoleStyler.styleOutput("Places to visit before any call to addMoreElements Method are: " + placesToVisit);
-        //addMoreElements(placesToVisit);
-        //ConsoleStyler.styleOutput("Places to visit after a call to LinkedLIst Add-Data Methods ( addFirst and addLast : addMoreElements ) are: " + placesToVisit);
-        //addMoreElements(placesToVisit);
-        //ConsoleStyler.styleOutput("Places to visit after a call to Queue Add-Data  Methods ( offer, offerFirst and offerLast: addMoreElements ) are: " + placesToVisit);
         addMoreElements(placesToVisit);
-        ConsoleStyler.styleOutput("Places to visit after a call to Stack Add-Data  Methods ( push : addMoreElements ) are: " + placesToVisit);
+        ConsoleStyler.styleOutput(PLACES_TO_VISIT_AFTER_A_CALL_TO + " LinkedLIst Add-Data Methods ( addFirst and addLast : addMoreElements ) are: " + placesToVisit);
+        addMoreElements(placesToVisit);
+        ConsoleStyler.styleOutput(PLACES_TO_VISIT_AFTER_A_CALL_TO + " Queue Add-Data  Methods ( offer, offerFirst and offerLast: addMoreElements ) are: " + placesToVisit);
+        addMoreElements(placesToVisit);
+        ConsoleStyler.styleOutput(PLACES_TO_VISIT_AFTER_A_CALL_TO + " Stack Add-Data  Methods ( push : addMoreElements ) are: " + placesToVisit);
         ConsoleStyler.divider();
         ConsoleStyler.styleOutput("Places to visit before any call to removeElements Method are: " + placesToVisit);
         removeElements(placesToVisit);
-        ConsoleStyler.styleOutput("Places to visit after a call to LinkedList Remove-Data Methods ( remove and remove(index-4) : removeElements ) are: \n "
+        ConsoleStyler.styleOutput(PLACES_TO_VISIT_AFTER_A_CALL_TO + " LinkedList Remove-Data Methods ( remove and remove(index-4) : removeElements ) are: \n "
                 + placesToVisit);
 
         if (!placesToVisit.isEmpty()) removeElements(placesToVisit);
-        ConsoleStyler.styleOutput("Places to visit after a call to Queue Remove-Data Methods ( remove, removeFirst and removeLast : removeElements ) are: \n "
+        ConsoleStyler.styleOutput(PLACES_TO_VISIT_AFTER_A_CALL_TO + " Queue Remove-Data Methods ( remove, removeFirst and removeLast : removeElements ) are: \n "
                 + placesToVisit);
         removeElements(placesToVisit);
-        ConsoleStyler.styleOutput("Places to visit after a call to Queue Remove-Data Methods ( remove, removeFirst and removeLast : removeElements ) are: \n "
+        ConsoleStyler.styleOutput(PLACES_TO_VISIT_AFTER_A_CALL_TO + " Queue Remove-Data Methods ( remove, removeFirst and removeLast : removeElements ) are: \n "
                 + placesToVisit);
         removeElements(placesToVisit);
-        ConsoleStyler.styleOutput("Places to visit after a call to Stack Remove-Data Methods ( pop : removeElements ) are: \n "
+        ConsoleStyler.styleOutput(PLACES_TO_VISIT_AFTER_A_CALL_TO + " Stack Remove-Data Methods ( pop : removeElements ) are: \n "
                 + placesToVisit);
         ConsoleStyler.divider();
 
@@ -52,7 +55,7 @@ public class LinkedListVsArrayList {
 
         //Queue Methods
         placesToVisit.offer("Melbourne");
-        placesToVisit.offerFirst("Brisbane");
+        placesToVisit.offerFirst(BRISBANE);
         placesToVisit.offerLast("Toowoomba");
         // Stack Methods
         placesToVisit.push("Alice Springs");
@@ -62,32 +65,32 @@ public class LinkedListVsArrayList {
     private static void removeElements(LinkedList<String> placesToVisit) {
         if (placesToVisit.size() >= 5) {
             placesToVisit.remove(4);
-            placesToVisit.remove("Brisbane");
+            placesToVisit.remove(BRISBANE);
         }
         ConsoleStyler.styleEachAsIs(null, placesToVisit);
         ConsoleStyler.styleOutput("" + placesToVisit.size());
         //Queue removal methods
         String s1 = placesToVisit.remove();
-        ConsoleStyler.styleOutput(s1 + " was removed from placesToVisit.");
+        ConsoleStyler.styleOutput(s1 + " " + WAS_REMOVED_FROM_PLACES_TO_VISIT);
         String s2 = placesToVisit.removeFirst();
-        ConsoleStyler.styleOutput(s2 + " was removed from placesToVisit.");
+        ConsoleStyler.styleOutput(s2 + " " + WAS_REMOVED_FROM_PLACES_TO_VISIT);
         if(!placesToVisit.isEmpty()) {
             String s3 = placesToVisit.removeLast();
-            ConsoleStyler.styleOutput(s3 + " was removed from placesToVisit.");
+            ConsoleStyler.styleOutput(s3 + " " + WAS_REMOVED_FROM_PLACES_TO_VISIT);
         }
 
         //Additional LinkedList Removal Methods:
         String p1 = placesToVisit.poll(); // remove first
-        ConsoleStyler.styleOutput(p1 + " was removed from placesToVisit.");
+        ConsoleStyler.styleOutput(p1 + " " + WAS_REMOVED_FROM_PLACES_TO_VISIT);
         String p2 = placesToVisit.pollFirst(); // remove first
-        ConsoleStyler.styleOutput(p2 + " was removed from placesToVisit.");
+        ConsoleStyler.styleOutput(p2 + " " + WAS_REMOVED_FROM_PLACES_TO_VISIT);
         String p3 = placesToVisit.pollLast();
-        ConsoleStyler.styleOutput(p3 + " was removed from placesToVisit.");
+        ConsoleStyler.styleOutput(p3 + " " + WAS_REMOVED_FROM_PLACES_TO_VISIT);
         ConsoleStyler.styleOutput("Places to visit after polling methods is: :\n"
                 + placesToVisit);
         //Stack push and pop methods
         placesToVisit.push("Sydney");
-        placesToVisit.push("Brisbane");
+        placesToVisit.push(BRISBANE);
         placesToVisit.push("Canberra");
         ConsoleStyler.styleOutput("Places to visit after pushing new elements are:\n"
                 + placesToVisit);
