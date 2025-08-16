19706eb450db6890f30f08f8e3392b48ca2dc01c
Hemant Bellani <hemantbellanilearns77@gmail.com>
2025-08-16 22:57:11 +0530

    SONAR-FIX

--- src/main/java/com/hb/study/udemy_lpa_javamasterclass/section7_oop_part1/coding_challenges/constructorchallengeexercise/MainClass.java
+++ src/main/java/com/hb/study/udemy_lpa_javamasterclass/section7_oop_part1/coding_challenges/constructorchallengeexercise/MainClass.java
@@ -1,5 +1,6 @@
 package com.hb.study.udemy_lpa_javamasterclass.section7_oop_part1.coding_challenges.constructorchallengeexercise;
 
+import com.hb.study.udemy_lpa_javamasterclass.global.constants.CommonConstants;
 import com.hb.study.udemy_lpa_javamasterclass.global.utils.ConsoleStyler;
 
 public class MainClass {
@@ -8,29 +9,27 @@ public class MainClass {
         //Test 1: Calling Constructor with 3 arguments
 
         Customer firstCustomer = new Customer("Tim", 20000000.00, "tim@gmail.com");
-        ConsoleStyler.styleOutput("First Customer's details are: \n" + firstCustomer +
-                "\n******************************************************************************************************************************************************");
-        /*ConsoleStyler.styleOutput(customer.getName());
-        ConsoleStyler.styleOutput(customer.getCredit_limit());
-        ConsoleStyler.styleOutput(customer.getEmailaddress());*/
-        //ConsoleStyler.styleOutput(firstCustomer);
-
+        ConsoleStyler.styleOutput("First Customer's details are: \n" + firstCustomer);
+        ConsoleStyler.styleOutput(firstCustomer.getName());
+        ConsoleStyler.styleOutput(firstCustomer.getCredit_limit() + CommonConstants.EMPTYSTRING);
+        ConsoleStyler.styleOutput(firstCustomer.getEmailaddress());
+        ConsoleStyler.styleOutput(firstCustomer.toString());
+        ConsoleStyler.divider();
         //Test 2: Calling Empty Constructor
         Customer secondCustomer = new Customer();
-        ConsoleStyler.styleOutput("Second Customer's details are:  \n" + secondCustomer +
-                "\n******************************************************************************************************************************************************");
-/*        ConsoleStyler.styleOutput(secondCustomer.getName());
-        ConsoleStyler.styleOutput(secondCustomer.getCredit_limit());
-        ConsoleStyler.styleOutput(secondCustomer.getEmailaddress());*/
-        //ConsoleStyler.styleOutput(secondCustomer);
-
+        ConsoleStyler.styleOutput("Second Customer's details are:  \n" + secondCustomer);
+        ConsoleStyler.styleOutput(secondCustomer.getName());
+        ConsoleStyler.styleOutput(secondCustomer.getCredit_limit() + CommonConstants.EMPTYSTRING);
+        ConsoleStyler.styleOutput(secondCustomer.getEmailaddress());
+        ConsoleStyler.styleOutput(secondCustomer.toString());
+        ConsoleStyler.divider();
         //Test 3: Calling Constructor with 2 arguments
         Customer thirdCustomer = new Customer("Hemant", "hemantbellani@gmail.com");
-        ConsoleStyler.styleOutput("Third Customer's details are:  \n" + thirdCustomer +
-                "\n******************************************************************************************************************************************************");
-     /*   ConsoleStyler.styleOutput(thirdCustomer.getName());
-        ConsoleStyler.styleOutput(thirdCustomer.getCredit_limit());
-        ConsoleStyler.styleOutput(thirdCustomer.getEmailaddress());*/
-        //ConsoleStyler.styleOutput(thirdCustomer);
+        ConsoleStyler.styleOutput("Third Customer's details are:  \n" + thirdCustomer);
+        ConsoleStyler.styleOutput(thirdCustomer.getName());
+        ConsoleStyler.styleOutput(thirdCustomer.getCredit_limit() + CommonConstants.EMPTYSTRING);
+        ConsoleStyler.styleOutput(thirdCustomer.getEmailaddress());
+        ConsoleStyler.styleOutput(thirdCustomer.toString());
+        ConsoleStyler.divider();
     }
 }
