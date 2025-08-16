1e3fee95e2970866d0319a33df435ef718fcb4a5
Hemant Bellani <hemantbellanilearns77@gmail.com>
2025-08-16 23:08:24 +0530

    SONAR-FIX

--- src/main/java/com/hb/study/udemy_lpa_javamasterclass/section7_oop_part1/demostubs/inheriranceexample/InheritanceExample_MainClass.java
+++ src/main/java/com/hb/study/udemy_lpa_javamasterclass/section7_oop_part1/demostubs/inheriranceexample/InheritanceExample_MainClass.java
@@ -2,7 +2,7 @@ package com.hb.study.udemy_lpa_javamasterclass.section7_oop_part1.demostubs.inhe
 
 import com.hb.study.udemy_lpa_javamasterclass.global.utils.ConsoleStyler;
 
-public class MainClass {
+public class InheritanceExample_MainClass {
     public static void main(String[] args)  {
         Animal firstGenericAnimal = new Animal("Generic Animal", "Huge", 400);
         doAnimalStuff(firstGenericAnimal, "slow");
@@ -15,21 +15,22 @@ public class MainClass {
         doAnimalStuff(yorkie, "fast");
         yorkie.move("fast");
         yorkie.makeNoise();
-        ConsoleStyler.styleOutput("x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x\n");
+        ConsoleStyler.divider();
 
         Dog retriever = new Dog("Labrador Retriever", 65, "Floppy", "Swimmer");
         doAnimalStuff(retriever, "slow");
         retriever.makeNoise();
         retriever.move("slow");
-        ConsoleStyler.styleOutput("x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x\n");
+        ConsoleStyler.divider();
 
         Dog wolf = new Dog("Wolf", 40);
         doAnimalStuff(wolf, "slow");
-        ConsoleStyler.styleOutput("x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x\n");
+        ConsoleStyler.divider();
 
         Fish goldie = new Fish("GoldFish", 0.25, 2, 3);
         doAnimalStuff(goldie, "fast");
-        ConsoleStyler.styleOutput("x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x");
+        ConsoleStyler.divider();
+
 
 
 
