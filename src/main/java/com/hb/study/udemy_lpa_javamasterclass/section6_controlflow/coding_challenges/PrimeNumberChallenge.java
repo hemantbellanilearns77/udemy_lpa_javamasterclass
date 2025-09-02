cd5bc2cadc0a5cdade75161d47724d6aa10211a4
Hemant Bellani <hemantbellanilearns77@gmail.com>
2025-09-02 07:02:38 +0530

    sonar-fixes

--- src/main/java/com/hb/study/udemy_lpa_javamasterclass/section6_controlflow/coding_challenges/PrimeNumberChallenge.java
+++ src/main/java/com/hb/study/udemy_lpa_javamasterclass/section6_controlflow/coding_challenges/PrimeNumberChallenge.java
@@ -9,61 +9,66 @@ public class PrimeNumberChallenge {
         for(int wholeNumberCounter = 0; wholeNumberCounter <= 50; wholeNumberCounter++) {
             if(isPrime(wholeNumberCounter)) {
                 primeNumbersCounter++;
-                System.out.print(wholeNumberCounter + ", ");
+                ConsoleStyler.styleOutput(wholeNumberCounter + ", ");
                 if(primeNumbersCounter == 3){
                     ConsoleStyler.styleOutput("Since we found 3 Prime numbers, so exiting... ");
                     break;
                 } else {
-                    //ConsoleStyler.styleOutput(wholeNumberCounter + " IS NOT A Prime Number, but a Composite Number or 0 or 1");
-                    continue;
+                    ConsoleStyler.styleExecutionInsight(wholeNumberCounter + """
+                            IS NOT A Prime Number, but a Composite Number or 0 or 1.
+                            Hence continuing to loop till loop limit is reached.
+                            """);
+
                 }
             }
         }
             ConsoleStyler.styleOutput("\nThe total count of Prime Numbers found are: " + primeNumbersCounter);
+            ConsoleStyler.styleExecutionInsight("""
+                    If you have access to code of this program, here's an potentially alternative implementation:
+                    /*public static boolean isPrimeNumber(int wholeNumber){
+                            //boolean numberisPrime = true;
+                            int loopCounter = 0;
+                            if(wholeNumber <= 2) {
+                                //return numberisPrime;
+                                return true;
+                            }
+                            else {
+                                if (wholeNumber % 2 != 0) {
+                                    loopCounter = wholeNumber / 2;
+                                    for (; loopCounter > 1; loopCounter--) {
+                                        if ((wholeNumber % loopCounter) == 0) {
+                                            return false;
+                                            *//*numberisPrime = false;
+                                            break;*//*
+                                        }
+                                    }
+                                } else {
+                                    //numberisPrime = false;
+                                    return false;
+                                }
+                            }
+                            return true;
+                            //return numberisPrime;
+                        }*/
+                    """);
         }
 
 
     public static boolean isPrime(int wholeNumber){
-        //boolean numberisPrime = true;
-        int loopCounter = 0;
+        boolean numberisPrime = true;
         if(wholeNumber < 3) {
-            //return numberisPrime;
-            return false;
+            numberisPrime = false;
         }
         else {
             for (int divisor = 2; divisor <= wholeNumber/2; divisor++) {
-                if(wholeNumber % divisor == 0) {
-                    return false;
+                if (wholeNumber % divisor == 0) {
+                    numberisPrime = false;
+                    break;
                 }
             }
         }
-        return true;
-        //return numberisPrime;
+        return numberisPrime;
     }
 
-    /*public static boolean isPrimeNumber(int wholeNumber){
-        //boolean numberisPrime = true;
-        int loopCounter = 0;
-        if(wholeNumber <= 2) {
-            //return numberisPrime;
-            return true;
-        }
-        else {
-            if (wholeNumber % 2 != 0) {
-                loopCounter = wholeNumber / 2;
-                for (; loopCounter > 1; loopCounter--) {
-                    if ((wholeNumber % loopCounter) == 0) {
-                        return false;
-                        *//*numberisPrime = false;
-                        break;*//*
-                    }
-                }
-            } else {
-                //numberisPrime = false;
-                return false;
-            }
-        }
-        return true;
-        //return numberisPrime;
-    }*/
+
 }
