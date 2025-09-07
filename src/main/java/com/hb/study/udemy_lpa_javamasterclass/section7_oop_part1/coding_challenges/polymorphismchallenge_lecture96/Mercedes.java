e87abeb06e425b5d76187851234426aaa6db4139
Hemant Bellani <hemantbellanilearns77@gmail.com>
2025-09-07 16:01:17 +0530

    Sonar Fixes

--- src/main/java/com/hb/study/udemy_lpa_javamasterclass/section7_oop_part1/coding_challenges/polymorphismchallenge_lecture96/Mercedes.java
+++ src/main/java/com/hb/study/udemy_lpa_javamasterclass/section7_oop_part1/coding_challenges/polymorphismchallenge_lecture96/Mercedes.java
@@ -3,12 +3,17 @@ package com.hb.study.udemy_lpa_javamasterclass.section7_oop_part1.coding_challen
  import com.hb.study.udemy_lpa_javamasterclass.global.utils.ConsoleStyler;
 public class Mercedes extends Car{
     public Mercedes(int cylinders, String name) {
+
         super(cylinders,name);
     }
+    @Override
     public void startEngine() {
+
         ConsoleStyler.styleOutput("startEngine() inside Mercedes class invoked");
     }
+    @Override
     public void accelerate() {
+
         ConsoleStyler.styleOutput("accelerate() inside Mercedes class invoked");
     }
 }
