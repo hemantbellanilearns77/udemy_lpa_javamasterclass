ce7e5c19071882e1d076431efeb4284611533e78
Hemant Bellani <hemantbellanilearns77@gmail.com>
2025-09-07 16:15:25 +0530

    Sonar Fixes

--- src/main/java/com/hb/study/udemy_lpa_javamasterclass/section12_generics/demostubs/generics_basics/BaseballTeam.java
+++ src/main/java/com/hb/study/udemy_lpa_javamasterclass/section12_generics/demostubs/generics_basics/BaseballTeam.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public class BaseballTeam {
 
-    private String teamName;
+    private final String teamName;
     private List<BaseballPlayer> teamMembers = new ArrayList<>();
     private int totalWins = 0;
     private int totalLosses = 0;
