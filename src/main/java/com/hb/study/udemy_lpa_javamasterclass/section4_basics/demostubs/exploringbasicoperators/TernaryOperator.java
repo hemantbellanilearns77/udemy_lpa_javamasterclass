55bc2e594ff3547343062b63e698673155a58f7a
Hemant Bellani <hemantbellanilearns77@gmail.com>
2025-09-11 10:58:58 +0530

    Sonar Fixes

--- src/main/java/com/hb/study/udemy_lpa_javamasterclass/section4_basics/demostubs/exploringbasicoperators/TernaryOperator.java
+++ src/main/java/com/hb/study/udemy_lpa_javamasterclass/section4_basics/demostubs/exploringbasicoperators/TernaryOperator.java
@@ -2,18 +2,27 @@ package com.hb.study.udemy_lpa_javamasterclass.section4_basics.demostubs.explori
 
 import com.hb.study.udemy_lpa_javamasterclass.global.utils.ConsoleStyler;
 
+import java.util.Arrays;
+
 public class TernaryOperator {
         public static void main(String[] args) {
-        //
+            if(args!= null && args.length>0) {
+                ConsoleStyler.styleExecutionInsight("args as supplied are: " + Arrays.toString(args));
+            }
         ConsoleStyler.styleOutput("******** Inside TernaryOperator Class..... *********");
         String makeOfCar = "Volkswagen";
+        ConsoleStyler.styleExecutionInsight("""
+                Was:
+                boolean isDomestic = !makeOfCar.equals("Volkswagen");
+                if(isDomestic) {
+                    ConsoleStyler.styleOutput("This car is domestic to our country");
+                } else {
+                    ConsoleStyler.styleOutput("This car is domestic to our country");
+                }
+                Modified to make code sonar friendly
+                """);
 
-        boolean isDomestic = makeOfCar == "Volkswagen" ? false : true ;
-        if(isDomestic) {
-            ConsoleStyler.styleOutput("This car is domestic to our country");
-        } else {
-            ConsoleStyler.styleOutput("This car is domestic to our country");
+            ConsoleStyler.styleOutput("This car of make: " + makeOfCar + " is domestic to our country");
+
         }
-
-    }
 }
