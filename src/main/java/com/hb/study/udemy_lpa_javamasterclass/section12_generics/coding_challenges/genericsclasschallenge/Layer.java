58ba71e3523d5d645b6e736cfeee68a9f515b06e
Hemant Bellani <hemantbellanilearns77@gmail.com>
2025-09-12 07:50:53 +0530

    Sonar Fixes

--- src/main/java/com/hb/study/udemy_lpa_javamasterclass/section12_generics/coding_challenges/genericsclasschallenge/Layer.java
+++ src/main/java/com/hb/study/udemy_lpa_javamasterclass/section12_generics/coding_challenges/genericsclasschallenge/Layer.java
@@ -5,13 +5,14 @@ import java.util.List;
 
 public class Layer <T extends Mappable> {
 
-    private List<T> layerElements;
+    private final List<T> layerElements;
 
     public Layer(T[] layerElements) {
-        this.layerElements = new ArrayList<T>(List.of(layerElements));
+        this.layerElements = new ArrayList<>(List.of(layerElements));
     }
 
-    public void addElements(T... elements) {
+    @SafeVarargs
+    public final void addElements(T... elements) {
         layerElements.addAll(List.of(elements));
     }
 
