13ab504c5b47f1b06d7c92c016c413790b915d27
hemantbellanilearns77 <hemantbellanlearns77@gmail.com>
2025-09-26 18:10:20 +0530

    Sonar Fixes

--- src/main/java/com/hb/study/udemy_lpa_javamasterclass/section5_buildingblocks/exercises/exercise12/PlayingCat.java
+++ src/main/java/com/hb/study/udemy_lpa_javamasterclass/section5_buildingblocks/exercises/exercise12/PlayingCat.java
@@ -13,18 +13,15 @@ public class PlayingCat {
 
     public static boolean isCatPlaying(boolean summer, int temperature) {
         boolean catPlayingStatus = false;
-        //if its summer i.e. if temp is between 25 and 45 (inclusive) - return true
-        // if winter i.e. it's NOT summer and temp is between 25 and 35 (inclusive) - return true
-        // return false in rest of the  scenarios
 
         if(temperature >= 25) {
-            if((summer && temperature <= 45 ) || (!summer && temperature <= 35) {
-                catPlayingStatus = true;
-            } else if (){
+            //if its summer i.e. if temp is between 25 and 45 (inclusive) - return true
+            // if winter i.e. it's NOT summer and temp is between 25 and 35 (inclusive) - return true
+            // return false in rest of the  scenarios
+            if((summer && temperature <= 45 ) || (!summer && temperature <= 35)) {
                 catPlayingStatus = true;
             }
         }
-
         return catPlayingStatus;
     }
 }
