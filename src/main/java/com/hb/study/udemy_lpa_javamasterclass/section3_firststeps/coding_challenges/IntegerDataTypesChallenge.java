3d49c5b8c0a5294809e71739dc4c4f206b395e17
hemantbellanilearns77 <hemantbellanlearns77@gmail.com>
2025-09-26 22:58:48 +0530

    Sonar - Fixes

--- src/main/java/com/hb/study/udemy_lpa_javamasterclass/section3_firststeps/coding_challenges/IntegerDataTypesChallenge.java
+++ src/main/java/com/hb/study/udemy_lpa_javamasterclass/section3_firststeps/coding_challenges/IntegerDataTypesChallenge.java
@@ -1,18 +1,23 @@
 package com.hb.study.udemy_lpa_javamasterclass.section3_firststeps.coding_challenges;
 
 import com.hb.study.udemy_lpa_javamasterclass.global.utils.ConsoleStyler;
+import com.hb.study.udemy_lpa_javamasterclass.global.utils.ExecutionUtil;
 
 /**
  * PrimitiveTypesChallange1
  */
 public class IntegerDataTypesChallenge {
-        public static void main(String[] args) {
-        //
+    public static final ExecutionUtil execution = new ExecutionUtil();
+
+    public static void main(String[] args) {
+        execution.initialize(args);
         byte byteVar = 77;
         short shortVar = 777;
         int intVar = 7777;
 
         long longVar = (50000L + (10L * (byteVar + shortVar + intVar)));
-        ConsoleStyler.styleOutput("Sum of " + 50000 + " with " + 10 + " times sum of byteVar, shortVar and intVar is: " + longVar);
+        ConsoleStyler.styleOutput("Sum of " + 50000 + " with " + 10 +
+                " times sum of byteVar, shortVar and intVar is: " + longVar);
+        execution.finalizeExecution();
     }
 }
