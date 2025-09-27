c18e1bfa6269467bd237fdbd9bbf84e12e6e843e
hemantbellanilearns77 <hemantbellanlearns77@gmail.com>
2025-09-27 07:06:29 +0530

    Sonar - Fixes

--- src/main/java/com/hb/study/udemy_lpa_javamasterclass/section6_controlflow/coding_challenges/ReadUserInputChallenge.java
+++ src/main/java/com/hb/study/udemy_lpa_javamasterclass/section6_controlflow/coding_challenges/ReadUserInputChallenge.java
@@ -1,13 +1,14 @@
 package com.hb.study.udemy_lpa_javamasterclass.section6_controlflow.coding_challenges;
 
 import com.hb.study.udemy_lpa_javamasterclass.global.utils.ConsoleStyler;
+import com.hb.study.udemy_lpa_javamasterclass.global.utils.ExecutionUtil;
 
 import java.util.Scanner;
 
 public class ReadUserInputChallenge {
-
+    private static final ExecutionUtil execution = new ExecutionUtil();
         public static void main(String[] args) {
-        //
+        execution.initialize(args);
 
         Scanner scanner = new Scanner(System.in);
         int countOfIntegers = 0;
@@ -27,15 +28,17 @@ public class ReadUserInputChallenge {
             }
         }
         ConsoleStyler.styleOutput("The sum of " + countOfIntegers + " integers as input above is : " + sum);
+        execution.finalizeExecution();
     }
     public static boolean isValidInteger(String nextNumberInput){
 
         boolean isValidInt = true;
-
+        int nextNumber;
         try {
-            int nextNumber = Integer.parseInt(nextNumberInput);
+            nextNumber = Integer.parseInt(nextNumberInput);
+            ConsoleStyler.styleExecutionInsight("You entered: " + nextNumber + " and that's a valid Input!");
         } catch(NumberFormatException numberFormatException){
-            ConsoleStyler.styleOutput("Invalid Input");
+            ConsoleStyler.styleExecutionInsight("You entered: " + nextNumberInput + " and that's an Invalid Input!");
             isValidInt = false;
             return isValidInt;
         }
